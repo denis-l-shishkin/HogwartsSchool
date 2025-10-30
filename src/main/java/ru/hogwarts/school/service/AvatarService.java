f0ad4f1473ee55480bc45490deba06e07296e125
denis-l-shishkin <79815076666@ya.ru>
2025-10-30 19:56:04 +0300

    HW 4.4. Initial commit.

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +23,9 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 @Transactional
 public class AvatarService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AvatarService.class);
+
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
 
@@ -44,6 +49,7 @@ public class AvatarService {
                 BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
         ) {
             bis.transferTo(bos);
+            logger.info("Avatar uploaded successfully");
         }
         Avatar avatar = findOrCreateAvatar(studentId);
         avatar.setStudent(student);
@@ -57,17 +63,21 @@ public class AvatarService {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
     public Avatar findAvatar(Long studentId) {
+        logger.debug("Finding avatar for student: " + studentId);
         return avatarRepository.findByStudentId(studentId).orElseThrow();
     }
     public Avatar findOrCreateAvatar(Long studentId) {
+        logger.info("Finding and create avatar for student: " + studentId);
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
 
     public Page<Avatar> getAllAvatars(Integer page, Integer size) {
         if (page > 0 && size > 0) {
             Pageable pageable = PageRequest.of(page - 1, size);
+            logger.debug("Finding avatars for student");
             return avatarRepository.findAll(pageable);
         }
+        logger.error("Page must be > 0 or size must be > 0");
         throw new IllegalArgumentException("Недопустимое значение страницы");
     }
 }
